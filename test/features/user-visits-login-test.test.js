b60038707fc939dfbd0656ac80d179f75402a298
joehou <tyranthou@gmail.com>
2018-09-02 17:10:20 -0700

    added chai to dev dep and first failing test for login page feature

--- test/features/user-visits-login-test.test.js
+++ test/features/user-visits-login-test.test.js
@@ -1,14 +1,32 @@
-const {assert} = require('chai')
-const request = require('supertest')
+const puppeteer =require("puppeteer")
 const  {buildUserRegistration} = require('../test-utils')
 
+let page
+let browser
+const width = 1920 
+const height = 1080
+
+beforeAll(async () => {
+    browser = await puppeteer.launch({
+          headless: true,
+          slowMo: 80, 
+          args: [`--window-size=${width},${height}`]
+        },100000);
+    page = await browser.newPage();
+    await page.setViewport({ width, height });
+});
+afterAll(() => {
+    browser.close();
+});
 
 describe("User visits login page", () => {
-  describe("starts with empty login", () =>{
-    it("starts with cursor focused in empty email field", () =>{
-      const  expectedFocus = 'userEmail'
-      
-      expect(expectedFocus).toBe('userEmail')
-    })
+  describe("starts with empty login on login page", () =>{
+    it("Has the correct title", async () =>{
+      const  expectedTitle = 'Login'
+      await page.goto("http://localhost:3000/");
+      // await page.waitForSelector('.welcome-message');
+      const title = await page.title()
+      expect(title).toBe(expectedTitle)
+    },100000)
   })
 })
