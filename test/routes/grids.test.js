cfbf0a1d79eaee3c5b0cb54bbbc00f2fb86ba819
joehou <emailjoehou@gmail.com>
2019-12-17 13:32:07 -0800

    updated test to check for more than just event name update

--- test/routes/grids.test.js
+++ test/routes/grids.test.js
@@ -13,11 +13,15 @@ describe("The grids api for users /api/users/:id/grids", () =>{
   // /api/users/:user_id/grids/:grid_id/events/:event_id
   describe("post api/users/:id/grids/:grid_id/events/:event_id",() => {
     test("updates event and returns the updated event", async () => {
+      const newEvent= {"eventName":"My first events update","eventColor":"lightBlue","eventFontColor":"Black","startBlock":0,"endBlock":7,"_id":"5c25552a6936d241c2f1bbbb"}
       const res = await request("http://localhost:3000")
         .put('/api/users/JohnDough20/grids/5c25552a6936d241c2f1bbba/events/5c25552a6936d241c2f1bbbb')
+        .send(newEvent)
         .expect(200)
         .expect('Content-Type',/json/)
-      expect(res.body).toBe('My Starter Grid')
+      expect(res.body.eventName).toBe('My first events update')
+      expect(res.body.eventColor).toBe('lightBlue')
+
     },10000)
   })
 })
